82c379473d98e3bdb03ed4fdfe7247d799283488
coopslarhette <coopslarhette@gmail.com>
2019-03-29 12:32:58 -0700

    adds bottom up LCS completely working, collectSolution needs tweaks

--- src/lcs/LCS.java
+++ src/lcs/LCS.java
@@ -1,61 +1,121 @@
 package lcs;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
-    
+
     /**
      * memoCheck is used to verify the state of your tabulation after
      * performing bottom-up and top-down DP. Make sure to set it after
      * calling either one of topDownLCS or bottomUpLCS to pass the tests!
      */
     public static int[][] memoCheck;
-    
+
     // -----------------------------------------------
     // Shared Helper Methods
     // -----------------------------------------------
-    
+
     // [!] TODO: Add your shared helper methods here!
-    
+
+    private static Set<String> collectSolution(String rStr, int r, String cStr, int c, int[][] memo) {
+        Set<String> tempSet;
+        Set<String> result = new HashSet<>();
+        if (r == 0 || c == 0) {
+            return new HashSet<>(Collections.singletonList(""));
+        } else if (rStr.charAt(r) == cStr.charAt(c)) {
+            tempSet = collectSolution(rStr, r - 1, cStr, c - 1, memo);
+            for (String str : tempSet) {  //will this copy the string or pass pointer thru? maybe cause bug
+                str += rStr.charAt(r);
+            }
+            return tempSet;
+        } else {
+            if (memo[r][c-1] >= memo[r-1][c]) {
+                result.addAll(collectSolution(rStr, r, cStr, c-1, memo));
+            }
+            if (memo[r - 1][c] >= memo[r][c-1]) {
+                result.addAll(collectSolution(rStr, r-1, cStr, c, memo));
+            }
+            return result;
+        }
+
+    }
+
 
     // -----------------------------------------------
     // Bottom-Up LCS
     // -----------------------------------------------
-    
+
     /**
      * Bottom-up dynamic programming approach to the LCS problem, which
      * solves larger and larger subproblems iterative using a tabular
      * memoization structure.
+     *
      * @param rStr The String found along the table's rows
      * @param cStr The String found along the table's cols
      * @return The longest common subsequence between rStr and cStr +
-     *         [Side Effect] sets memoCheck to refer to table
+     * [Side Effect] sets memoCheck to refer to table
      */
-    public static Set<String> bottomUpLCS (String rStr, String cStr) {
-        throw new UnsupportedOperationException();
+    public static Set<String> bottomUpLCS(String rStr, String cStr) {  //works
+        int[][] table = new int[rStr.length() + 1][cStr.length() + 1];
+        fillFirstZeros(table);
+        System.out.println("table at fillzero\n" + Arrays.deepToString(table));
+        for (int r = 1; r < rStr.length() + 1; r++) {
+            for (int c = 1; c < cStr.length() + 1; c++) {
+                if (rStr.charAt(r-1) == cStr.charAt(c-1)) {
+                    table[r][c] = 1 + table[r - 1][c - 1];
+                } else {
+                    table[r][c] = Math.max(table[r - 1][c], table[r][c - 1]);
+                }
+            }
+        }
+        memoCheck = table;
+        System.out.println("table right before return: \n" + Arrays.deepToString(table));
+        System.out.println(collectSolution(rStr, rStr.length() - 1, cStr, cStr.length() - 1, table).toString());  //prints whole set
+        return collectSolution(rStr, rStr.length() - 1, cStr, cStr.length() - 1, table);  //should it be length - 1?
     }
-    
+
     // [!] TODO: Add any bottom-up specific helpers here!
-    
-    
+
+    /**
+     * Takes a bottom-up table and fills first row and column with zeroes to make the programming a little
+     * easier
+     *
+     * @param table Table to fill. Note: assumes a single row length for all rows and signle column length
+     *              for all columns
+     */
+    private static void fillFirstZeros(int[][] table) {
+        for (int r = 0; r < table.length; r++) {
+            table[r][0] = 0;
+        }
+        for (int c = 0; c < table[0].length; c++) {
+            table[0][c] = 0;
+        }
+    }
+
+
     // -----------------------------------------------
     // Top-Down LCS
     // -----------------------------------------------
-    
+
     /**
      * Top-down dynamic programming approach to the LCS problem, which
      * solves smaller and smaller subproblems recursively using a tabular
      * memoization structure.
+     *
      * @param rStr The String found along the table's rows
      * @param cStr The String found along the table's cols
      * @return The longest common subsequence between rStr and cStr +
-     *         [Side Effect] sets memoCheck to refer to table  
+     * [Side Effect] sets memoCheck to refer to table
      */
-    public static Set<String> topDownLCS (String rStr, String cStr) {
+    public static Set<String> topDownLCS(String rStr, String cStr) {
+
         throw new UnsupportedOperationException();
     }
-    
+
     // [!] TODO: Add any top-down specific helpers here!
-    
-    
+
+
 }
